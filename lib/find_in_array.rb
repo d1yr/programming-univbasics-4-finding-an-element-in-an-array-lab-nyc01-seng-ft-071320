dd38d4cb4fb40c77346c9b6f1f55e91b72709744
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-07-10 00:48:21 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,8 +3,9 @@ def find_element_index(array, value_to_find)
   found_value_index = nil
   while count < array.length do
     if array[count] == value_to_find
-    count += 1 
     found_value_index = count
   end
-  found_value_index
+     count += 1 
+   end
+   found_value_index
 end
